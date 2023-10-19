f3aa5ec56f898b5514dadd991509864cbc4ed749
manuelcastro2 <malavermanuelricardo@gmail.com>
2023-10-19 12:42:38 -0500

    ccifrado de clave

--- proyecto/src/main/java/com/nuevastecnologias/uts/proyecto/Services/Implements/usuariosServiceImpl.java
+++ proyecto/src/main/java/com/nuevastecnologias/uts/proyecto/Services/Implements/usuariosServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.nuevastecnologias.uts.proyecto.Entitys.Usuarios;
 import com.nuevastecnologias.uts.proyecto.Repository.usuariosRepository;
 import com.nuevastecnologias.uts.proyecto.Services.UsuarioService;
+import java.security.MessageDigest;
 
 @Service
 public class usuariosServiceImpl implements UsuarioService {
@@ -23,7 +24,27 @@ public class usuariosServiceImpl implements UsuarioService {
 
     @Override
     public Usuarios save(Usuarios usuarios) {
+        usuarios.setPassword(hash(usuarios.getPassword()));
         return usuariosrepository.save(usuarios);
+    }
+
+    public static String hash(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(input.getBytes("UTF-8"));
+
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -33,20 +54,20 @@ public class usuariosServiceImpl implements UsuarioService {
 
     @Override
     public Usuarios update(Usuarios usuarios, Long id) {
-         Optional<Usuarios> terceroOptional = usuariosrepository.findById(id);
-      
-      if (terceroOptional.isPresent()) {
-          Usuarios terceroCurrent = terceroOptional.get();
-          terceroCurrent.setDocument(usuarios.getDocument());
-          terceroCurrent.setName(usuarios.getName());
-          terceroCurrent.setLastname(usuarios.getLastname());
-          terceroCurrent.setRole(usuarios.getRole());
-          terceroCurrent.setPassword(usuarios.getPassword());
-          usuariosrepository.save(terceroCurrent);
-          return terceroCurrent;
-      } else {
-          return null;
-      }
+        Optional<Usuarios> terceroOptional = usuariosrepository.findById(id);
+
+        if (terceroOptional.isPresent()) {
+            Usuarios terceroCurrent = terceroOptional.get();
+            terceroCurrent.setDocument(usuarios.getDocument());
+            terceroCurrent.setName(usuarios.getName());
+            terceroCurrent.setLastname(usuarios.getLastname());
+            terceroCurrent.setRole(usuarios.getRole());
+            terceroCurrent.setPassword(usuarios.getPassword());
+            usuariosrepository.save(terceroCurrent);
+            return terceroCurrent;
+        } else {
+            return null;
+        }
     }
 
     @Override
